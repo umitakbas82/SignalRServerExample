964010fda5a229125910ad56326d236b36976ebc
umitakbas82 <umit.akbas@hotmail.com.tr>
2024-02-22 17:19:30 +0300

    clientsource and chathub class

---
+++ Data/ClientSource.cs
@@ -0,0 +1,13 @@
+﻿using SignalRServerExample.Models;
+using System.Collections.Generic;
+
+namespace SignalRServerExample.Data
+{
+    public class ClientSource
+    {
+        public static List<Client> clients = new List<Client>();
+
+        public static List<Client> Clients { get;  }=new List<Client>();
+       
+    }
+}
--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Connections.Features;
+using Microsoft.AspNetCore.SignalR;
+using SignalRServerExample.Data;
+using SignalRServerExample.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +10,16 @@ namespace SignalRServerExample.Hubs
 {
     public class ChatHub:Hub
     {
-        public async Task
+        public async Task GetNickName(string nickName)
+        {
+            Client client = new Client()
+            {
+                ConnectionId = Context.ConnectionId,
+                NickName = nickName
+            };
+
+            ClientSource.Clients.Add(client);
+            Clients.Others.SendAsync("clientJoined", nickName);
+        }
     }
 }
